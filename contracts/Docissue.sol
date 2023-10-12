91bb1955cf195202e7586618889cc803c735aab4
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-10-12 18:55:46 -0400

    Added upload doc function

--- contracts/Docissue.sol
+++ contracts/Docissue.sol
@@ -9,32 +9,31 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Docissue is SismoConnect, ERC721Holder {
     bytes16 private _appId = 0xd6e0a23df3d426bf3b5f232ff4c69058;
-    uint256 public tableId;
+    uint256 public issuerTableId;
+    uint256 public docTableId;
     string private constant _TABLE_1_PREFIX = "issuers";
     string private constant _TABLE_2_PREFIX = "documents";
-    bool private _isImpersonationMode = true;
+    // bool private _isImpersonationMode = true;
 
-    constructor() SismoConnect(buildConfig(_appId, _isImpersonationMode)) {
+    constructor() SismoConnect(buildConfig(_appId)) {
       // Create a new issuers table to store issuer details
-      tableId = TablelandDeployments.get().create(
+      issuerTableId = TablelandDeployments.get().create(
         address(this),
         SQLHelpers.toCreateFromSchema(
           "id integer primary key," // Notice the trailing comma
-          "name text,"
-          "website text,"
-          "description text,"
+
           "twitter text",
           _TABLE_1_PREFIX
         ));
 
       // Create a new documents table to store the cid and metadata of the documents
-      tableId = TablelandDeployments.get().create(
+      docTableId = TablelandDeployments.get().create(
         address(this),
         SQLHelpers.toCreateFromSchema(
           "id integer primary key," // Notice the trailing comma
           "name text,"
-          "cid text,"
-          "type int,",
+          "cid text,",
+          // "type int,",
           _TABLE_2_PREFIX
         ));
     }
@@ -62,10 +61,10 @@ contract Docissue is SismoConnect, ERC721Holder {
         // Create a new issuer by inserting their details into the issuers table
         TablelandDeployments.get().mutate(
           address(this),
-          tableId,
+          issuerTableId,
           SQLHelpers.toInsert(
             _TABLE_1_PREFIX,
-            tableId,
+            issuerTableId,
             "id,name,website,description,twitter",
             string.concat(
               "1,",
@@ -76,6 +75,48 @@ contract Docissue is SismoConnect, ERC721Holder {
               SQLHelpers.quote(description),
               ",",
               SQLHelpers.quote(Strings.toHexString(twitterId)) // Wrap strings in single quotes with the `quote` method
+            )
+          )
+        );
+    }
+
+    // Create a new document for the dApp
+    function uploadDocument(
+        // bytes memory sismoConnectResponse,
+        string memory title,
+        string memory cid
+        // int8 docType
+    ) public {
+        // SismoConnectVerifiedResult memory result = verify({
+        //     responseBytes: sismoConnectResponse,
+        //     // we want users to prove that they own a Twitter account
+        //     // we are recreating the auth request made in the frontend to be sure that
+        //     // the proofs provided in the response are valid with respect to this auth request
+        //     auth: buildAuth({authType: AuthType.TWITTER})
+        // });
+
+        // uint256 twitterId = SismoConnectHelper.getUserId(
+        //     result,
+        //     AuthType.TWITTER
+        // );
+
+        // Create a new issuer by inserting their details into the issuers table
+        TablelandDeployments.get().mutate(
+          address(this),
+          docTableId,
+          SQLHelpers.toInsert(
+            _TABLE_1_PREFIX,
+            docTableId,
+            "id,title,cid,docType",
+            string.concat(
+              "1,",
+              SQLHelpers.quote(title),
+              ",",
+              SQLHelpers.quote(cid)
+            //   ",",
+              // docType,
+              // ",",
+            //   SQLHelpers.quote(Strings.toHexString(twitterId)) // Wrap strings in single quotes with the `quote` method
             )
           )
         );
